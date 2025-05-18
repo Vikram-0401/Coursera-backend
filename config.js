2f0cafcf6e78ee9d042ff6198763e03bd88a599f
vikram <vikramshettyr4@gmail.com>
2025-05-18 07:56:44 +0530

    Added middleware

---
+++ config.js
@@ -0,0 +1,7 @@
+const JWT_USER_PASSWORD = 'vikram';
+const JWT_ADMIN_PASSWORD = 'shetty';
+
+module.exports = {
+    JWT_ADMIN_PASSWORD,
+    JWT_USER_PASSWORD
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-const { adminModel } = require('../db');
+const { adminModel, courseModel } = require('../db');
 const adminRouter = Router();
 const jwt = require('jsonwebtoken');
-const JWT_ADMIN_PASSWORD = 'shetty';
+const { JWT_ADMIN_PASSWORD } = require('../config')
+const { adminMiddleware } = require('../middleware/admin')
 
 adminRouter.post('/signup', async function(req, res) {
     const { email, password, firstName, lastName } = req.body;
@@ -46,9 +47,21 @@ adminRouter.post('/signin', async function(req, res) {
 });
 
 
-adminRouter.post('/course', function(req, res) {
+adminRouter.post('/course', adminMiddleware, async function(req, res) {
+    const adminId = req.userId;
+
+    const { title, description, imageUrl, price } = req.body;
+
+    const course = await courseModel.create({
+        title: title,
+        description: description,
+        imageUrl: imageUrl,
+        price: price,
+        creatorId: adminId
+    })
+
     res.json({
-        msg: "Create a course"
+        msg: "Created a course"
     });
 });
 
